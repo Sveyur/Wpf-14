2a46a380de767ad08cee89ea19f6cac9d15af563
smolkovasg@yandex.ru <smolkovasg@yandex.ru>
2022-01-06 19:14:46 +0300

    Добавьте файлы проекта.

---
+++ Wpf-14/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Каравай",
+                ProductPrice = 50,
+                ProductImage = "/Data/karavay.jpg",
+                ProductCategory = ProductCategories.Еда
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Батон",
+                ProductPrice = 36,
+                ProductImage = "/Data/loaf.jpg",
+                ProductCategory = ProductCategories.Еда
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Платье",
+                ProductPrice = 5600,
+                ProductImage = "/Data/dress.jpg",
+                ProductCategory = ProductCategories.Одежда
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Фен",
+                ProductPrice = 2800,
+                ProductImage = "/Data/fen.jpg",
+                ProductCategory = ProductCategories.БытоваяТехника
+            });
+            product.ItemsSource = products;
+        }
+
+
+    }
+}
---
+++ Wpf-14/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_14
+{
+    public enum ProductCategories
+    {
+        Еда,
+        Одежда,
+        БытоваяТехника
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public uint ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public ProductCategories ProductCategory { get; set; }
+        
+    }
+}
+
